17b967912130d24dc0c5a1a57135b3f9ce9c8669
fengzhu2013 <913346548@qq.com>
2018-07-09 06:34:43 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,14 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\User;
 
 class UsersController extends Controller
 {
-
-
     public function create()
     {
         return view('users.create');
+    }
+
+
+
+    /**
+     * 展示用户注册页面
+     * @param  User   $user [description]
+     * @return [type]       [description]
+     */
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+
+    public function store(Request $request)
+    {
+        //数据校验
+        $this->validate($request, [
+            'name'      => 'required|max:50',
+            'email'     => 'required|email|unique:users|max:255',
+            'password'  => 'required|confirmed|min:6'
+        ]);
+
+
+        //插入数据库
+        $user = User::create([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => bcrypt($request->password),
+        ]);
+
+        if (is_null($user)) {
+            //提示错误信息
+        }
+
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+        //重定向
+        return redirect()->route('users.show',[$user]);
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('/about','StaticPagesController@about')->name('about');
 
 
 Route::get('signup','UsersController@create')->name('signup');
+
+
+Route::resource('users','UsersController');
